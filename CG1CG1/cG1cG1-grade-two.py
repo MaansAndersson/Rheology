b69723c749fbe287c1bf878c0d9d04d0e6dede3c
Måns Andersson <mansandersson@Manss-MacBook-Air.local>
2020-05-28 13:01:09 +0200

    revert norm

--- CG1CG1/cG1cG1-grade-two.py
+++ CG1CG1/cG1cG1-grade-two.py
@@ -113,10 +113,10 @@ for i in range(0,15):
     solve(rt==0, sigma)
     
     
-    Uoldr.vector().axpy(-1, U.vector())
-    relup = norm(Uoldr,'H1')
-    relup /= norm(U,'H1')
-    #relup = errornorm(u,u0,norm_type='H1',degree_rise=2)/norm(u,norm_type='H1')
+    #u0.vector().axpy(-1, u.vector())
+    #relup = norm(u0,'H1')
+    #relup /= norm(u,'H1')
+    relup = errornorm(u,u0,norm_type='H1',degree_rise=2)/norm(u,norm_type='H1')
     print("u update: ", relup)
     if relup < 1e-3: #(u0.vector().axpy(-1.0, u.vector()).norm("linf") / u.vector().norm("linf")) < 1e-4:
         print ("newton iteration: ", i, "Divergance: ", assemble(sqrt(div(u) * div(u))*dx)) #, " Rel change",
